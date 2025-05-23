912a9ac01a497cdd39c37bd408981559d931686b
satriodwi47 <satriodwiu47@gmail.com>
2025-05-23 22:43:43 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3049,6 +3049,9 @@ document.addEventListener('DOMContentLoaded', function () {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 }
+    }
+    ]
+};                        
 // Data GeoJSON untuk fasilitas sampah
 var wasteFacilitiesData = {
     "type": "FeatureCollection",
